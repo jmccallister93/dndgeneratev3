1997e3aa6805290f014dabd9777e1719a609752c
jmccallister93 <jaredmccallister93@gmail.com>
2022-11-11 15:56:18 -0800

    Added drop down menus

--- src/pages/NpcGen.js
+++ src/pages/NpcGen.js
@@ -1,54 +1,100 @@
 import Navbar from "../components/Navbar";
-import style from "../stylesheets/NpcGen.module.scss"
-import { Dropdown } from 'primereact/dropdown';
+import style from "../stylesheets/NpcGen.module.scss";
+import { Dropdown } from "primereact/dropdown";
+import { useState } from "react";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 const NpcGen = () => {
-    const options = {
-        url: ''
-    }
+  const [race, setRace] = useState(null);
+  const [sex, setSex] = useState(null);
+  const [align, setAlign] = useState(null);
 
-    const fieldsRace = [
-        {label: 'Aasimar', value: 'aasimar'},
-        {label: 'Deep Gnome', value: 'deep gnome'},
-        {label: 'Dragonborn', value: 'dragonborn'},
-        {label: 'Duergar', value: 'duergar'},
-        {label: 'Drow', value: 'drow'},
-        {label: 'Firbolg', value: 'firbolg'},
-        {label: 'Genasi', value: 'genasi'},
-        {label: 'Gnome', value: 'gnome'},
-        {label: 'Golbin', value: 'goblin'},
-        {label: 'Goliath', value: 'goliath'},
-        {label: 'Half-Elf', value: 'half-elf'},
-        {label: 'Half-Orc', value: 'half-orc'},
-        {label: 'High Elf', value: 'high elf'},
-        {label: 'Hill Dwarf', value: 'hill dwarf'},
-        {label: 'Hobgoblin ', value: 'stout halfling'},       
-        {label: 'Human', value: 'human'},
-        {label: 'Kenku', value: 'kenku'},
-        {label: 'Kobold', value: 'kobold'},
-        {label: 'Lightfoot Halfling', value: 'lightfoot halfling'},
-        {label: 'Lizardfolk', value: 'Lizardfolk'},
-        {label: 'Mountain Dwarf', value: 'mountain dwarf'},
-        {label: 'Orc', value: 'orc'},
-        {label: 'Tabaxi', value: 'tabaxi'},
-        {label: 'Tiefling', value: 'tiefling'},
-        {label: 'Triton', value: 'triton'},
-        {label: 'Stout Halfling', value: 'stout halfling'},
-        {label: 'Wood Elf', value: 'wood elf'},
-        {label: 'Yuan-Ti', value: 'yuan-ti'},
-    ]
-    const cities = [
-        { name: 'New York', code: 'NY' },
-        { name: 'Rome', code: 'RM' },
-        { name: 'London', code: 'LDN' },
-        { name: 'Istanbul', code: 'IST' },
-        { name: 'Paris', code: 'PRS' }
-    ];
+  const races = [
+    {name: "Random", value: "random"},
+    { name: "Aasimar", value: "aasimar" },
+    { name: "Deep Gnome", value: "deep gnome" },
+    { name: "Dragonborn", value: "dragonborn" },
+    { name: "Duergar", value: "duergar" },
+    { name: "Drow", value: "drow" },
+    { name: "Firbolg", value: "firbolg" },
+    { name: "Genasi", value: "genasi" },
+    { name: "Gnome", value: "gnome" },
+    { name: "Golbin", value: "goblin" },
+    { name: "Goliath", value: "goliath" },
+    { name: "Half-Elf", value: "half-elf" },
+    { name: "Half-Orc", value: "half-orc" },
+    { name: "High Elf", value: "high elf" },
+    { name: "Hill Dwarf", value: "hill dwarf" },
+    { name: "Hobgoblin ", value: "stout halfling" },
+    { name: "Human", value: "human" },
+    { name: "Kenku", value: "kenku" },
+    { name: "Kobold", value: "kobold" },
+    { name: "Lightfoot Halfling", value: "lightfoot halfling" },
+    { name: "Lizardfolk", value: "Lizardfolk" },
+    { name: "Mountain Dwarf", value: "mountain dwarf" },
+    { name: "Orc", value: "orc" },
+    { name: "Tabaxi", value: "tabaxi" },
+    { name: "Tiefling", value: "tiefling" },
+    { name: "Triton", value: "triton" },
+    { name: "Stout Halfling", value: "stout halfling" },
+    { name: "Wood Elf", value: "wood elf" },
+    { name: "Yuan-Ti", value: "yuan-ti" },
+  ];
+
+  const sexes = [
+    {name: "Random", value: "random"},
+    {name: "Male", value: "male"},
+    {name:"Female", value: "female"}
+  ]
+
+  const aligns =[
+    {name: "Random", value: "random"},
+    {name: "Good", value: "good"},
+    {name: "Neutral", value: "neutral"},
+    {name: "Evil", value: "evil"},
+  ]
+
+  const onRaceChange = (e) => {
+    setRace(e.value);
+  };
+  
+  const onSexChange = (e) => {
+    setSex(e.value);
+  };
+
+  const onAlignChange = (e) => {
+    setAlign(e.value);
+  };
+
   return (
     <div className={style.npcgenWrapper}>
       <Navbar />
       <h1>Race</h1>
-      <Dropdown label="Race" options={fieldsRace} placeholder="Select Race"/>
+      <Dropdown
+        optionLabel="name"
+        value={race}
+        options={races}
+        onChange={onRaceChange}
+        placeholder="Random"
+      />
+      <h1>Sex</h1>
+      <Dropdown
+        optionLabel="name"
+        value={sex}
+        options={sexes}
+        onChange={onSexChange}
+        placeholder="Random"
+      />
+      <h1>Alignment</h1>
+      <Dropdown
+        optionLabel="name"
+        value={align}
+        options={aligns}
+        onChange={onAlignChange}
+        placeholder="Random"
+      />
     </div>
   );
 };
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,36 @@
 import Navbar from "../components/Navbar";
+import { Dropdown } from 'primereact/dropdown';
+import { useState } from "react";
 
 const SignUp = () => {
+    
+    const [selectedCity1, setSelectedCity1] = useState(null);
+    
+    const cities = [
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' }
+    ];
+
+    const citySelectItems = [
+        {label: 'New York', value: 'NY'},
+        {label: 'Rome', value: 'RM'},
+        {label: 'London', value: 'LDN'},
+        {label: 'Istanbul', value: 'IST'},
+        {label: 'Paris', value: 'PRS'}
+    ];
+ 
+    const onCityChange = (e) => {
+        setSelectedCity1(e.value);
+    }
+
     return ( 
         <div className="signup">
            <Navbar />
+           <h5>Basic</h5>
+                <Dropdown value={selectedCity1} options={cities} onChange={onCityChange} optionLabel="name" placeholder="Select a City" />
         </div>
      );
 }
