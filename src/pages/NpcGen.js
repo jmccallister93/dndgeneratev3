7ae6ba501e09dac558840c09b9454bba256e465d
jmccallister93 <jaredmccallister93@gmail.com>
2022-11-11 14:40:34 -0800

    Added race list

--- src/pages/NpcGen.js
+++ src/pages/NpcGen.js
@@ -1,11 +1,54 @@
 import Navbar from "../components/Navbar";
 import style from "../stylesheets/NpcGen.module.scss"
+import { Dropdown } from 'primereact/dropdown';
 
 const NpcGen = () => {
+    const options = {
+        url: ''
+    }
+
+    const fieldsRace = [
+        {label: 'Aasimar', value: 'aasimar'},
+        {label: 'Deep Gnome', value: 'deep gnome'},
+        {label: 'Dragonborn', value: 'dragonborn'},
+        {label: 'Duergar', value: 'duergar'},
+        {label: 'Drow', value: 'drow'},
+        {label: 'Firbolg', value: 'firbolg'},
+        {label: 'Genasi', value: 'genasi'},
+        {label: 'Gnome', value: 'gnome'},
+        {label: 'Golbin', value: 'goblin'},
+        {label: 'Goliath', value: 'goliath'},
+        {label: 'Half-Elf', value: 'half-elf'},
+        {label: 'Half-Orc', value: 'half-orc'},
+        {label: 'High Elf', value: 'high elf'},
+        {label: 'Hill Dwarf', value: 'hill dwarf'},
+        {label: 'Hobgoblin ', value: 'stout halfling'},       
+        {label: 'Human', value: 'human'},
+        {label: 'Kenku', value: 'kenku'},
+        {label: 'Kobold', value: 'kobold'},
+        {label: 'Lightfoot Halfling', value: 'lightfoot halfling'},
+        {label: 'Lizardfolk', value: 'Lizardfolk'},
+        {label: 'Mountain Dwarf', value: 'mountain dwarf'},
+        {label: 'Orc', value: 'orc'},
+        {label: 'Tabaxi', value: 'tabaxi'},
+        {label: 'Tiefling', value: 'tiefling'},
+        {label: 'Triton', value: 'triton'},
+        {label: 'Stout Halfling', value: 'stout halfling'},
+        {label: 'Wood Elf', value: 'wood elf'},
+        {label: 'Yuan-Ti', value: 'yuan-ti'},
+    ]
+    const cities = [
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' }
+    ];
   return (
     <div className={style.npcgenWrapper}>
       <Navbar />
-      NPC GEN
+      <h1>Race</h1>
+      <Dropdown label="Race" options={fieldsRace} placeholder="Select Race"/>
     </div>
   );
 };
