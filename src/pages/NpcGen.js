03d3e93c7767b0e7f1035c12f03fbd9261f108c4
jmccallister93 <jaredmccallister93@gmail.com>
2022-11-14 13:23:01 -0800

    Added more tables and features of npcgen

--- src/pages/NpcGen.js
+++ src/pages/NpcGen.js
@@ -8,81 +8,239 @@ import "primeicons/primeicons.css";
 import supabase from "../config/supabaseClient";
 
 const NpcGen = () => {
-  const [race, setRace] = useState(null);
-  const [raceOptions, setRaceOptions] = useState()
-  const [sex, setSex] = useState("Random");
-  const [align, setAlign] = useState("Random");
-  const [fetchError, setFetchError] = useState(null)
-  const [races, setRaces] = useState(null)
+  const [fetchError, setFetchError] = useState(null);
+
+  const [abilities, setAbilities] = useState();
+  const [abilitiesOptions, setAbilitiesOptions] = useState();
+  const [highAbility, setHighAbility] = useState();
+  const [lowAbility, setLowAbility] = useState();
+
+  const [align, setAlign] = useState(null);
+  const [aligns, setAligns] = useState();
+  const [alignOptions, setAlignOptions] = useState();
+
+  const [bond, setBond] = useState();
+  const [bonds, setBonds] = useState();
+  const [bondOptions, setBondOptions] = useState();
+
+  const [feature, setFeature] = useState();
+  const [features, setFeatures] = useState();
+  const [featureOptions, setFeatureOptions] = useState();
+
+  const [interaction, setInteraction] = useState();
+  const [interactions, setInteractions] = useState();
+  const [interactionOptions, setInteractionOptions] = useState();
+
+  const [prof, setProf] = useState();
+  const [profs, setProfs] = useState();
+  const [profOptions, setProfOptions] = useState();
   
-  
+  const [mannerism, setMannerism] = useState();
+  const [mannerisms, setMannerisms] = useState();
+  const [mannerismOptions, setMannerismOptions] = useState();
+
+  const [race, setRace] = useState();
+  const [races, setRaces] = useState();
+  const [raceOptions, setRaceOptions] = useState();
+
+  const [sex, setSex] = useState(null);
+  const [sexes, setSexes] = useState();
+  const [sexOptions, setSexOptions] = useState();
+
+  const [talent, setTalent] = useState();
+  const [talents, setTalents] = useState();
+  const [talentOptions, setTalentOptions] = useState();
+
   useEffect(() => {
     const fetchData = async () => {
-      const { data, error } = await supabase
-        .from('races')
-        .select()
-           
+      const { data, error } = await supabase.from("abilities").select();
       if (error) {
-        setFetchError('Could not fetch the data')
-        setRaces(null)
-        console.log(error)
+        setFetchError("Could not fetch the data");
+        setAbilities(null);
+        console.log(error);
       }
       if (data) {
-        setRaces(data)
-        setFetchError(null)
-        setRaceOptions(data.map(r => (
-          {name:r.name, value: r.value}
-          )))
+        setAbilities(data);
+        setFetchError(null);
+        setAbilitiesOptions(
+          data.map((r) => ({ name: r.name, value: r.value }))
+        );
       }
-    }
+    };
+    fetchData();
+  }, []);
 
-    fetchData()
-  }, []) 
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("aligns").select();
+      if (error) {
+        setFetchError("Could not fetch the data");
+        setAlign(null);
+        console.log(error);
+      }
+      if (data) {
+        setAligns(data);
+        setFetchError(null);
+        setAlignOptions(
+          data.map((r) => ({ name: r.name, value: r.value }))
+        );
+      }
+    };
+    fetchData();
+  }, []);
 
-  // const races = [
-  //   { name: "Random", value: "Random" },
-  //   { name: "Aasimar", value: "Aasimar" },
-  //   { name: "Deep Gnome", value: "Deep Gnome" },
-  //   { name: "Dragonborn", value: "Dragonborn" },
-  //   { name: "Duergar", value: "Duergar" },
-  //   { name: "Drow", value: "Drow" },
-  //   { name: "Firbolg", value: "Firbolg" },
-  //   { name: "Genasi", value: "Genasi" },
-  //   { name: "Gnome", value: "Gnome" },
-  //   { name: "Golbin", value: "Goblin" },
-  //   { name: "Goliath", value: "Goliath" },
-  //   { name: "Half-Elf", value: "Half-Elf" },
-  //   { name: "Half-Orc", value: "Half-Orc" },
-  //   { name: "High Elf", value: "High Elf" },
-  //   { name: "Hill Dwarf", value: "Hill Dwarf" },
-  //   { name: "Hobgoblin ", value: "Stout Halfling" },
-  //   { name: "Human", value: "Human" },
-  //   { name: "Kenku", value: "Kenku" },
-  //   { name: "Kobold", value: "Kobold" },
-  //   { name: "Lightfoot Halfling", value: "Lightfoot Halfling" },
-  //   { name: "Lizardfolk", value: "Lizardfolk" },
-  //   { name: "Mountain Dwarf", value: "Mountain Dwarf" },
-  //   { name: "Orc", value: "Orc" },
-  //   { name: "Tabaxi", value: "Tabaxi" },
-  //   { name: "Tiefling", value: "Tiefling" },
-  //   { name: "Triton", value: "Triton" },
-  //   { name: "Stout Halfling", value: "Stout Halfling" },
-  //   { name: "Wood Elf", value: "Wood Elf" },
-  //   { name: "Yuan-Ti", value: "Yuan-Ti" },
-  // ];
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("bonds").select();
+      if (error) {
+        setFetchError("Could not fetch the data");
+        setBond(null);
+        console.log(error);
+      }
+      if (data) {
+        setBonds(data);
+        setFetchError(null);
+        setBondOptions(
+          data.map((r) => ({ name: r.name, value: r.value }))
+        );
+      }
+    };
+    fetchData();
+  }, []);
 
-  const sexes = [
-    { name: "Random", value: "Random" },
-    { name: "Male", value: "Male" },
-    { name: "Female", value: "Female" },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("features").select();
+      if (error) {
+        setFetchError("Could not fetch the data");
+        setFeature(null);
+        console.log(error);
+      }
+      if (data) {
+        setFeatures(data);
+        setFetchError(null);
+        setFeatureOptions(
+          data.map((r) => ({ name: r.name, value: r.value }))
+        );
+      }
+    };
+    fetchData();
+  }, []);
 
-  const aligns = [
-    { name: "Random", value: "Random" },
-    { name: "Good", value: "Good" },
-    { name: "Neutral", value: "Neutral" },
-    { name: "Evil", value: "Evil" },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("interactions").select();
+      if (error) {
+        setFetchError("Could not fetch the data");
+        setInteraction(null);
+        console.log(error);
+      }
+      if (data) {
+        setInteractions(data);
+        setFetchError(null);
+        setInteractionOptions(
+          data.map((r) => ({ name: r.name, value: r.value }))
+        );
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("mannerisms").select();
+      if (error) {
+        setFetchError("Could not fetch the data");
+        setMannerism(null);
+        console.log(error);
+      }
+      if (data) {
+        setMannerisms(data);
+        setFetchError(null);
+        setMannerismOptions(
+          data.map((r) => ({ name: r.name, value: r.value }))
+        );
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("professions").select();
+      if (error) {
+        setFetchError("Could not fetch the data");
+        setProf(null);
+        console.log(error);
+      }
+      if (data) {
+        setProfs(data);
+        setFetchError(null);
+        setProfOptions(
+          data.map((r) => ({ name: r.name, value: r.value }))
+        );
+      }
+    };
+    fetchData();
+  }, []);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("races").select();
+      if (error) {
+        setFetchError("Could not fetch the data");
+        setRaces(null);
+        console.log(error);
+      }
+      if (data) {
+        setRaces(data);
+        setRaceOptions(data.map((d) => ({ name: d.name, value: d.value })));
+        setFetchError(null);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("sexes").select();
+      
+      if (error) {
+        setFetchError("Could not fetch the data");
+        setSex(null);
+        console.log(error);
+      }
+      if (data) {
+        setSexes(data);
+        setSexOptions(
+          data.map((d) => ({ name: d.name, value: d.value }))
+        );
+        setFetchError(null);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("talents").select();
+      if (error) {
+        setFetchError("Could not fetch the data");
+        setTalent(null);
+        console.log(error);
+      }
+      if (data) {
+        setTalents(data);
+        setFetchError(null);
+        setTalentOptions(
+          data.map((r) => ({ name: r.name, value: r.value }))
+        );
+      }
+    };
+    fetchData();
+  }, []);
 
   const onRaceChange = (e) => {
     setRace(e.value);
@@ -96,9 +254,31 @@ const NpcGen = () => {
     setAlign(e.value);
   };
 
-  
-  
-    
+  const onProfChange = (e) => {
+    setProf(e.value);
+  };
+
+  const onFeatureChange = (e) => {
+    setFeature(e.value);
+  };
+  const onHighAbilityChange = (e) => {
+    setHighAbility(e.value);
+  };
+  const onLowAbilityChange = (e) => {
+    setLowAbility(e.value);
+  };
+  const onTalentChange = (e) => {
+    setTalent(e.value);
+  };
+  const onMannerismChange = (e) => {
+    setMannerism(e.value);
+  };
+  const onInteractionChange = (e) => {
+    setInteraction(e.value);
+  };
+  const onBondChange = (e) => {
+    setBond(e.value);
+  };
 
   return (
     <div className={style.npcgenWrapper}>
@@ -120,7 +300,7 @@ const NpcGen = () => {
             <Dropdown
               optionLabel="name"
               value={sex}
-              options={races}
+              options={sexOptions}
               onChange={onSexChange}
               placeholder="Random"
             />
@@ -130,16 +310,104 @@ const NpcGen = () => {
             <Dropdown
               optionLabel="name"
               value={align}
-              options={aligns}
+              options={alignOptions}
               onChange={onAlignChange}
+              placeholder="Random"
+            />
+          </div>
+          <div>
+            <h1>Profession</h1>
+            <Dropdown
+              optionLabel="name"
+              value={prof}
+              options={profOptions}
+              onChange={onProfChange}
+              placeholder="Random"
+            />
+          </div>
+          <div>
+            <h1>Feature</h1>
+            <Dropdown
+              optionLabel="name"
+              value={feature}
+              options={featureOptions}
+              onChange={onFeatureChange}
+              placeholder="Random"
+            />
+          </div>
+          <div>
+            <h1>High Ability</h1>
+            <Dropdown
+              optionLabel="name"
+              value={highAbility}
+              options={abilitiesOptions}
+              onChange={onHighAbilityChange}
+              placeholder="Random"
+            />
+          </div>
+          <div>
+            <h1>Low Ability</h1>
+            <Dropdown
+              optionLabel="name"
+              value={lowAbility}
+              options={abilitiesOptions}
+              onChange={onLowAbilityChange}
+              placeholder="Random"
+            />
+          </div>
+          <div>
+            <h1>Talent</h1>
+            <Dropdown
+              optionLabel="name"
+              value={talent}
+              options={talentOptions}
+              onChange={onTalentChange}
+              placeholder="Random"
+            />
+          </div>
+          <div>
+            <h1>Mannerism</h1>
+            <Dropdown
+              optionLabel="name"
+              value={mannerism}
+              options={mannerismOptions}
+              onChange={onMannerismChange}
+              placeholder="Random"
+            />
+          </div>
+          <div>
+            <h1>Interaction</h1>
+            <Dropdown
+              optionLabel="name"
+              value={interaction}
+              options={interactionOptions}
+              onChange={onInteractionChange}
+              placeholder="Random"
+            />
+          </div>
+          <div>
+            <h1>Bond</h1>
+            <Dropdown
+              optionLabel="name"
+              value={bond}
+              options={bondOptions}
+              onChange={onBondChange}
               placeholder="Random"
             />
           </div>
         </div>
         <div className={style.npcgenDisplay}>
-          <h1>{race}</h1>
-          <h1>{sex}</h1>
-          <h1>{align}</h1>
+          <h1>Race: {race}</h1>
+          <h1>Sex: {sex}</h1>
+          <h1>Alignment: {align}</h1>
+          <h1>Profession: {prof}</h1>
+          <h1>Feature: {feature}</h1>
+          <h1>High Ability: {highAbility}</h1>
+          <h1>Low Ability: {lowAbility}</h1>
+          <h1>Talent: {talent}</h1>
+          <h1>Mannerism: {mannerism}</h1>
+          <h1>Interaction: {interaction}</h1>
+          <h1>Bond: {bond}</h1>
         </div>
       </div>
     </div>
