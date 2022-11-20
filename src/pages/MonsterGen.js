2afc5564e41c0b1d3b9295589c2e88b77d4f2650
jmccallister93 <jaredmccallister93@gmail.com>
2022-11-20 01:26:36 -0800

    Displays

--- src/pages/MonsterGen.js
+++ src/pages/MonsterGen.js
@@ -10,110 +10,157 @@ import supabase from "../config/supabaseClient";
 import { e } from "mathjs";
 
 const MonsterGen = () => {
-    const [name, setName] = useState("");
-    const [names, setNames] = useState();
-    const [nameOptions, setNameOptions] = useState()
+  const [name, setName] = useState("");
+  const [names, setNames] = useState();
+  const [nameOptions, setNameOptions] = useState();
 
-    const [race, setRace] = useState("");
-    const [races, setRaces] = useState()
-    const [raceOptions, setRaceOptions] = useState()
+  const [race, setRace] = useState("");
+  const [races, setRaces] = useState();
+  const [raceOptions, setRaceOptions] = useState();
 
-    const [size, setSize] = useState("")
-    const [sizes, setSizes] = useState()
-    const [sizeOptions, setSizeOptions] = useState()
+  const [size, setSize] = useState("");
+  const [sizes, setSizes] = useState();
+  const [sizeOptions, setSizeOptions] = useState();
 
-    const [type, setType] = useState("")
-    const [types, setTypes] = useState()
-    const [typeOptions, setTypeOptions] = useState()
+  const [type, setType] = useState("");
+  const [types, setTypes] = useState();
+  const [typeOptions, setTypeOptions] = useState();
 
-    const [align, setAlign] = useState("")
-    const [aligns, setAligns] = useState()
-    const [alignOptions, setAlignOptions] = useState()
+  const [align, setAlign] = useState("");
+  const [aligns, setAligns] = useState();
+  const [alignOptions, setAlignOptions] = useState();
 
-    const [ac, setAc] = useState("")
-    const [acs, setAcs] = useState("")
-    const [acOptions, setAcOptions] = useState()
+  const [ac, setAc] = useState("");
+  const [acs, setAcs] = useState("");
+  const [acOptions, setAcOptions] = useState();
 
-    const [armorType, setArmorType] = useState("")
-    const [armorTypes, setArmorTypes] = useState()
-    const [armorTypeOptions, setArmorTypeOptions] = useState()
+  const [armorType, setArmorType] = useState("");
+  const [armorTypes, setArmorTypes] = useState();
+  const [armorTypeOptions, setArmorTypeOptions] = useState();
 
-    const [hp, setHp] = useState("")
-    const [hps, setHps] = useState("")
-    const [hpOptions, setHpOptions] = useState()
+  const [hp, setHp] = useState("");
+  const [hps, setHps] = useState("");
+  const [hpOptions, setHpOptions] = useState();
 
-    const [speed, setSpeed] = useState("")
-    const [speeds, setSpeeds] = useState("")
-    const [speedOptions, setSpeedOptions] = useState()
+  const [speed, setSpeed] = useState("");
+  const [speeds, setSpeeds] = useState("");
+  const [speedOptions, setSpeedOptions] = useState();
 
-    const [speedType, setSpeedType] = useState("")
-    const [speedTypes, setSpeedTypes] = useState("")
-    const [speedTypeOptions, setSpeedTypeOptions] = useState()
+  const [speedType, setSpeedType] = useState("");
+  const [speedTypes, setSpeedTypes] = useState("");
+  const [speedTypeOptions, setSpeedTypeOptions] = useState();
 
-    const [speedExtra, setSpeedExtra] = useState("")
-    const [speedExtras, setSpeedExtras] = useState("")
-    const [speedExtraOptions, setSpeedExtraOptions] = useState()
+  const [speedExtra, setSpeedExtra] = useState("");
+  const [speedExtras, setSpeedExtras] = useState("");
+  const [speedExtraOptions, setSpeedExtraOptions] = useState();
 
-    const [ability, setAbility] = useState("")
-    const [abilities, setAbilities] = useState("")
-    const [abilityOptions, setAbilityOptions] = useState()
+  const [ability, setAbility] = useState("");
+  const [abilities, setAbilities] = useState("");
+  const [abilityOptions, setAbilityOptions] = useState();
 
-    const [save, setSave] = useState("")
-    const [saves, setSaves] = useState("")
-    const [saveOptions, setSaveOptions] = useState()
+  const [save, setSave] = useState("");
+  const [saves, setSaves] = useState("");
+  const [saveOptions, setSaveOptions] = useState();
 
-    const [skill, setSkill] = useState("")
-    const [skills, setSkills] = useState("")
-    const [skillOptions, setSkillOptions] = useState()
+  const [skill, setSkill] = useState("");
+  const [skills, setSkills] = useState("");
+  const [skillOptions, setSkillOptions] = useState();
 
-    const [immune, setImmune] = useState("")
-    const [immunes, setImmunes] = useState("")
-    const [immuneOptions, setImmuneOptions] = useState()
+  const [vuln, setVuln] = useState("");
+  const [vulns, setVulns] = useState("");
+  const [vulnOptions, setVulnOptions] = useState();
 
-    const [resist, setResist] = useState("")
-    const [resists, setResists] = useState("")
-    const [resistOptions, setResistOptions] = useState()
+  const [immune, setImmune] = useState("");
+  const [immunes, setImmunes] = useState("");
+  const [immuneOptions, setImmuneOptions] = useState();
 
-    const [condition, setCondition] = useState("")
-    const [conditions, setConditions] = useState("")
-    const [conditionOptions, setConditionOptions] = useState()
+  const [resist, setResist] = useState("");
+  const [resists, setResists] = useState("");
+  const [resistOptions, setResistOptions] = useState();
 
-    const [sense, setSense] = useState("")
-    const [senses, setSenses] = useState("")
-    const [senseOptions, setSenseOptions] = useState()
+  const [condition, setCondition] = useState("");
+  const [conditions, setConditions] = useState("");
+  const [conditionOptions, setConditionOptions] = useState();
 
-    const [lang, setLang] = useState("")
-    const [langs, setLangs] = useState("")
-    const [langOptions, setLangOptions] = useState()
+  const [sense, setSense] = useState("");
+  const [senses, setSenses] = useState("");
+  const [senseOptions, setSenseOptions] = useState();
 
-    const [special, setSpecial] = useState("")
-    const [Specials, setSpecials] = useState("")
-    const [specialOptions, setSpecialOptions] = useState()
+  const [lang, setLang] = useState("");
+  const [langs, setLangs] = useState("");
+  const [langOptions, setLangOptions] = useState();
 
-    const [action, setAction] = useState("")
-    const [actions, setActions] = useState("")
-    const [actionOptions, setActionOptions] = useState()
+  const [special, setSpecial] = useState("");
+  const [Specials, setSpecials] = useState("");
+  const [specialOptions, setSpecialOptions] = useState();
 
-    const [legend, setLegend] = useState("")
-    const [legends, setLegends] = useState("")
-    const [legnedOptions, setLegendOptions] = useState()
+  const [action, setAction] = useState("");
+  const [actions, setActions] = useState("");
+  const [actionOptions, setActionOptions] = useState();
 
-    const [lair, setLair] = useState("")
-    const [Lairs, setLairs] = useState("")
-    const [lairOptions, setLairOptions] = useState()
+  const [legend, setLegend] = useState("");
+  const [legends, setLegends] = useState("");
+  const [legendOptions, setLegendOptions] = useState();
 
+  const [lair, setLair] = useState("");
+  const [Lairs, setLairs] = useState("");
+  const [lairOptions, setLairOptions] = useState();
 
-    const onNameChange = (e) => {
+  const [gear, setGear] = useState("");
+  const [gears, setGears] = useState("");
+  const [gearOptions, setGearOptions] = useState();
 
-    }
+  const onNameChange = (e) => {};
 
-    const onRandomName = (e) => {
+  const onRandomName = (e) => {};
 
-    }
+  const onRaceChange = (e) => {};
 
-    const onRaceChange = (e) => {
+  const onSizeChange = (e) => {};
 
-    }
+  const onTypeChange = (e) => {};
+
+  const onAlignChange = (e) => {};
+
+  const onAcChange = (e) => {};
+
+  const onArmorTypeChange = (e) => {};
+
+  const onHpChange = (e) => {};
+
+  const onSpeedChange = (e) => {};
+
+  const onSpeedTypeChange = (e) => {};
+
+  const onSpeedExtraChange = (e) => {};
+
+  const onAbilityChange = (e) => {};
+
+  const onSaveChange = (e) => {};
+
+  const onVulnChange = (e) => {};
+
+  const onSkillChange = (e) => {};
+
+  const onImmuneChange = (e) => {};
+
+  const onResistChange = (e) => {};
+
+  const onConditionChange = (e) => {};
+
+  const onSenseChange = (e) => {};
+
+  const onLangChange = (e) => {};
+
+  const onSpecialChange = (e) => {};
+
+  const onActionChange = (e) => {};
+
+  const onLegendChange = (e) => {};
+
+  const onLairChange = (e) => {};
+
+  const onGearChange = (e) => {};
 
   return (
     <div className={style.monstergenWrapper}>
@@ -121,14 +168,14 @@ const MonsterGen = () => {
       <div className={style.monstergenBody}>
         <h1 className={style.monstergenHeader}>Monster Generator</h1>
         <div className={style.monstergenOptionsWrapper}>
-            <div>
-                <h1>Name: </h1>
-                <InputText value={name} onChange={onNameChange}/>
-                <button onClick={onRandomName} className={style.monstergenBtnName}>
-                    Randomize
-                </button>
-            </div>
-            <div>
+          <div>
+            <h1>Name: </h1>
+            <InputText value={name} onChange={onNameChange} />
+            <button onClick={onRandomName} className={style.monstergenBtnName}>
+              Randomize
+            </button>
+          </div>
+          <div>
             <h1>Race</h1>
             <Dropdown
               optionLabel="name"
@@ -136,6 +183,236 @@ const MonsterGen = () => {
               options={raceOptions}
               onChange={onRaceChange}
               placeholder="Choose Race"
+            />
+          </div>
+          <div>
+            <h1>Size</h1>
+            <Dropdown
+              optionLabel="name"
+              value={size}
+              options={sizeOptions}
+              onChange={onSizeChange}
+              placeholder="Choose Size"
+            />
+          </div>
+          <div>
+            <h1>Type</h1>
+            <Dropdown
+              optionLabel="name"
+              value={type}
+              options={typeOptions}
+              onChange={onTypeChange}
+              placeholder="Choose Type"
+            />
+          </div>
+          <div>
+            <h1>Align</h1>
+            <Dropdown
+              optionLabel="name"
+              value={align}
+              options={alignOptions}
+              onChange={onAlignChange}
+              placeholder="Choose Alignment"
+            />
+          </div>
+          <div>
+            <h1>AC</h1>
+            <Dropdown
+              optionLabel="name"
+              value={ac}
+              options={acOptions}
+              onChange={onAcChange}
+              placeholder="Choose AC"
+            />
+          </div>
+          <div>
+            <h1>Armor Type</h1>
+            <Dropdown
+              optionLabel="name"
+              value={armorType}
+              options={armorTypeOptions}
+              onChange={onArmorTypeChange}
+              placeholder="Choose Armor Type"
+            />
+          </div>
+          <div>
+            <h1>HP</h1>
+            <Dropdown
+              optionLabel="name"
+              value={hp}
+              options={hpOptions}
+              onChange={onHpChange}
+              placeholder="Choose HP"
+            />
+          </div>
+          <div>
+            <h1>Speed</h1>
+            <Dropdown
+              optionLabel="name"
+              value={speed}
+              options={speedOptions}
+              onChange={onSpeedChange}
+              placeholder="Choose Speed"
+            />
+          </div>
+          <div>
+            <h1>Additional Movement</h1>
+            <Dropdown
+              optionLabel="name"
+              value={speedType}
+              options={speedTypeOptions}
+              onChange={onSpeedTypeChange}
+              placeholder="Choose Additional Move"
+            />
+          </div>
+          <div>
+            <h1>Additional Move Speed</h1>
+            <Dropdown
+              optionLabel="name"
+              value={speedExtra}
+              options={speedExtraOptions}
+              onChange={onSpeedExtraChange}
+              placeholder="Choose Additional Speed"
+            />
+          </div>
+          <div>
+            <h1>Abilities</h1>
+            <Dropdown
+              optionLabel="name"
+              value={ability}
+              options={abilityOptions}
+              onChange={onAbilityChange}
+              placeholder="Choose Ability"
+            />
+          </div>
+          <div>
+            <h1>Saves</h1>
+            <Dropdown
+              optionLabel="name"
+              value={save}
+              options={saveOptions}
+              onChange={onSaveChange}
+              placeholder="Choose Save"
+            />
+          </div>
+          <div>
+            <h1>Skill</h1>
+            <Dropdown
+              optionLabel="name"
+              value={skill}
+              options={skillOptions}
+              onChange={onSkillChange}
+              placeholder="Choose Skill"
+            />
+          </div>
+          <div>
+            <h1>Vulnerabities</h1>
+            <Dropdown
+              optionLabel="name"
+              value={vuln}
+              options={vulnOptions}
+              onChange={onVulnChange}
+              placeholder="Choose Vulnerabities"
+            />
+          </div>
+          <div>
+            <h1>Immunities</h1>
+            <Dropdown
+              optionLabel="name"
+              value={immune}
+              options={immuneOptions}
+              onChange={onImmuneChange}
+              placeholder="Choose Immunities"
+            />
+          </div>
+          <div>
+            <h1>Resistances</h1>
+            <Dropdown
+              optionLabel="name"
+              value={resist}
+              options={resistOptions}
+              onChange={onResistChange}
+              placeholder="Choose Resistances"
+            />
+          </div>
+          <div>
+            <h1>Condition Immunities</h1>
+            <Dropdown
+              optionLabel="name"
+              value={condition}
+              options={conditionOptions}
+              onChange={onConditionChange}
+              placeholder="Choose Condition Immunities"
+            />
+          </div>
+          <div>
+            <h1>Senses</h1>
+            <Dropdown
+              optionLabel="name"
+              value={sense}
+              options={senseOptions}
+              onChange={onSenseChange}
+              placeholder="Choose Senses"
+            />
+          </div>
+          <div>
+            <h1>Languages</h1>
+            <Dropdown
+              optionLabel="name"
+              value={lang}
+              options={langOptions}
+              onChange={onLangChange}
+              placeholder="Choose Languages"
+            />
+          </div>
+          <div>
+            <h1>Sepcials</h1>
+            <Dropdown
+              optionLabel="name"
+              value={special}
+              options={specialOptions}
+              onChange={onSpecialChange}
+              placeholder="Choose Specials"
+            />
+          </div>
+          <div>
+            <h1>Actions</h1>
+            <Dropdown
+              optionLabel="name"
+              value={action}
+              options={actionOptions}
+              onChange={onActionChange}
+              placeholder="Choose Actions"
+            />
+          </div>
+          <div>
+            <h1>Legendary Actions</h1>
+            <Dropdown
+              optionLabel="name"
+              value={legend}
+              options={legendOptions}
+              onChange={onLegendChange}
+              placeholder="Choose Legendary Actions"
+            />
+          </div>
+          <div>
+            <h1>Lair</h1>
+            <Dropdown
+              optionLabel="name"
+              value={lair}
+              options={lairOptions}
+              onChange={onLairChange}
+              placeholder="Choose Lair Actions"
+            />
+          </div>
+          <div>
+            <h1>Gear</h1>
+            <Dropdown
+              optionLabel="name"
+              value={gear}
+              options={gearOptions}
+              onChange={onGearChange}
+              placeholder="Choose Sear"
             />
           </div>
         </div>
