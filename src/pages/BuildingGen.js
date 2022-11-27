420c64a2f8fc8bc04652a476300174e98c923533
jmccallister93 <jaredmccallister93@gmail.com>
2022-11-27 11:10:44 -0800

    formatting building header

--- src/pages/BuildingGen.js
+++ src/pages/BuildingGen.js
@@ -9,9 +9,31 @@ import "primeicons/primeicons.css";
 import supabase from "../config/supabaseClient";
 
 const BuildingGen = () => {
+
+    const onGenerate = (e) => {}
+    const onClear = (e) => {}
+
     return ( 
      <div className={style.mainWrapper}>
         <Navbar />
+        <div className={style.topHeader}>
+        <h1 className={style.mainHeader}>Building Generator</h1>
+        {/* Generate Btns */}
+        <div>
+          <div className={style.btnWrapper}>
+            <button onClick={onGenerate} className={style.btnGen}>
+              Generate
+            </button>
+            <button onClick={onClear} className={style.btnClear}>
+              Clear
+            </button>
+          </div>
+        </div>
+      </div>
+        <div className={style.body}>
+        
+        <div className={style.optionsWrapper}></div>
+        </div>
      </div>   
     
     );
