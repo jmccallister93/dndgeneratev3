9f0934889cab9299e0ca17557bfac51d580a4876
jmccallister93 <jaredmccallister93@gmail.com>
2022-11-27 11:33:31 -0800

    Importing data

--- src/pages/BuildingGen.js
+++ src/pages/BuildingGen.js
@@ -9,14 +9,40 @@ import "primeicons/primeicons.css";
 import supabase from "../config/supabaseClient";
 
 const BuildingGen = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [buildingType, setBuildingType] = useState("");
+  const [buildingTypes, setBuildingTypes] = useState("");
+  const [buildingTypeOptions, setBuildingTypeOptions] = useState("");
 
-    const onGenerate = (e) => {}
-    const onClear = (e) => {}
+  const getData = (tableName, setSingular, setPlural, setOptions) => {
+    const fetchData = async () => {
+      const { data: dataName, error: errorName } = await supabase
+        .from(tableName)
+        .select();
+      if (errorName) {
+        setFetchError("Could not fetch the data");
+        console.log(errorName);
+        setSingular(null);
+      }
+      if (dataName) {
+        setPlural(dataName);
+        setFetchError(null);
+        setOptions(dataName.map((r) => ({ name: r.name, value: r.value })));
+      }
+    };
+    fetchData();
+  };
 
-    return ( 
-     <div className={style.mainWrapper}>
-        <Navbar />
-        <div className={style.topHeader}>
+  useEffect(() => {
+    getData("buildingsTypes", setBuildingType, setBuildingTypes, setBuildingTypeOptions);
+  }, []);
+  const onGenerate = (e) => {};
+  const onClear = (e) => {};
+
+  return (
+    <div className={style.mainWrapper}>
+      <Navbar />
+      <div className={style.topHeader}>
         <h1 className={style.mainHeader}>Building Generator</h1>
         {/* Generate Btns */}
         <div>
@@ -30,14 +56,13 @@ const BuildingGen = () => {
           </div>
         </div>
       </div>
-        <div className={style.body}>
-        
-        <div className={style.optionsWrapper}></div>
+      <div className={style.body}>
+        <div className={style.optionsWrapper}>
+            
         </div>
-     </div>   
-    
-    );
-   
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default BuildingGen;
