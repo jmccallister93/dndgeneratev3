addf5eed79bb042b9100d4cae81439796bd51b84
jmccallister93 <jaredmccallister93@gmail.com>
2022-12-02 15:50:37 -0800

    Got multiple states cleared

--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -3,7 +3,7 @@ import style from "../stylesheets/BuildingGen.module.scss";
 const ClearButton = (props) => {
   //Clear
   const onClear = (e) => {
-    for (let i = 0; props.amountOfStates > i; i++) {
+    for (let i = 0; props.setStringState.length > i; i++) {
         props.setStringState[i]("");
       }
     // props.setStringState("");
