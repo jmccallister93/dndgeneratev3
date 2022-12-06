cd2e4efef3c71f7cd5e08e5407b4b0754e516760
jmccallister93 <jaredmccallister93@gmail.com>
2022-12-06 00:02:24 -0800

    CityGen is template

--- src/components/GenerateButton.js
+++ src/components/GenerateButton.js
@@ -1,14 +1,26 @@
+import style from "../stylesheets/PageStyle.module.scss";
 const GenerateButton = (props) => {
-    const onGenerate = (e) => {
-        for (let i =0; i < props.generateItems.length; i++){
-            let max = props.generateItems[i].length
-            if(props.generateItems[i] === ""){
-                let r = Math.round(Math.random() * (max - 0))
-                props.setItem(props.itemOptions[r])
-            }
-        }
+  //----PROPS NEEDED----
+  //generateItems
+  //setItem
+  //itemOptions
+  //---PROPS NEEDED----
+  const onGenerate = (e) => {
+    for (let i = 0; i < props.generateItems.length; i++) {
+      let max = props.generateItems[i].length;
+      if (props.generateItems[i] === "") {
+        let r = Math.round(Math.random() * (max - 0));
+        props.setItem(props.itemOptions[r]);
+      }
     }
-return (<>{onGenerate}</>);
-}
- 
+  };
+  return (
+    <>
+      <button onClick={onGenerate} className={style.btnGen}>
+        Generate
+      </button>
+    </>
+  );
+};
+
 export default GenerateButton;
--- src/components/MultipleDisplay.js
+++ src/components/MultipleDisplay.js
@@ -1,5 +1,8 @@
 const MultipleDisplay = (props) => {
-    const multipleDisplay = props.selected.map((i) => {
+    //---PROPS NEEDED---
+    //selectedItem
+    //---PROPS NEEDED---
+    const multipleDisplay = props.selectedItem.map((i) => {
         return <span>{`${i.name},`}</span>;
       });
     return ( <>{multipleDisplay}</> );
