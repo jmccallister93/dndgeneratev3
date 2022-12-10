c48fb278af50de78e081f897809d65bf1f72a842
jmccallister93 <jaredmccallister93@gmail.com>
2022-12-10 13:02:12 -0800

    Displays classgen

--- src/pages/ClassGen.js
+++ src/pages/ClassGen.js
@@ -47,92 +47,92 @@ const ClassGen = () => {
   const [primaryAbility, setPrimaryAbility] = useState("");
   const [primaryAbilitys, setPrimaryAbilitys] = useState("");
   const [primaryAbilityOptions, setPrimaryAbilityOptions] = useState("");
-  const [primaryAbilityList, setPrimaryAbilityList] = useState("");
-  const [selectedPrimaryAbility, setSelectedPrimaryAbility] = useState("");
+  const [primaryAbilityList, setPrimaryAbilityList] = useState([]);
+  const [selectedPrimaryAbility, setSelectedPrimaryAbility] = useState([]);
 
   const [armorProf, setArmorProf] = useState("");
   const [armorProfs, setArmorProfs] = useState("");
   const [armorProfOptions, setArmorProfOptions] = useState("");
-  const [armorProfList, setArmorProfList] = useState("");
-  const [selectedArmorProf, setSelectedArmorProf] = useState("");
+  const [armorProfList, setArmorProfList] = useState([]);
+  const [selectedArmorProf, setSelectedArmorProf] = useState([]);
 
   const [weaponProf, setWeaponProf] = useState("");
   const [weaponProfs, setWeaponProfs] = useState("");
   const [weaponProfOptions, setWeaponProfOptions] = useState("");
-  const [weaponProfList, setWeaponProfList] = useState("");
-  const [selectedWeaponProf, setSelectedWeaponProf] = useState("");
+  const [weaponProfList, setWeaponProfList] = useState([]);
+  const [selectedWeaponProf, setSelectedWeaponProf] = useState([]);
 
   const [toolProf, setToolProf] = useState("");
   const [toolProfs, setToolProfs] = useState("");
   const [toolProfOptions, setToolProfOptions] = useState("");
-  const [toolProfList, setToolProfList] = useState("");
-  const [selectedToolProf, setSelectedToolProf] = useState("");
+  const [toolProfList, setToolProfList] = useState([]);
+  const [selectedToolProf, setSelectedToolProf] = useState([]);
 
   const [saveProf, setSaveProf] = useState("");
   const [saveProfs, setSaveProfs] = useState("");
   const [saveProfOptions, setSaveProfOptions] = useState("");
-  const [saveProfList, setSaveProfList] = useState("");
-  const [selectedSaveProf, setSelectedSaveProf] = useState("");
+  const [saveProfList, setSaveProfList] = useState([]);
+  const [selectedSaveProf, setSelectedSaveProf] = useState([]);
 
   const [skillProf, setSkillProf] = useState("");
   const [skillProfs, setSkillProfs] = useState("");
   const [skillProfOptions, setSkillProfOptions] = useState("");
-  const [skillProfList, setSkillProfList] = useState("");
-  const [selectedSkillProf, setSelectedSkillProf] = useState("");
+  const [skillProfList, setSkillProfList] = useState([]);
+  const [selectedSkillProf, setSelectedSkillProf] = useState([]);
 
   const [language, setLanguage] = useState("");
   const [languages, setLanguages] = useState("");
   const [languageOptions, setLanguageOptions] = useState("");
-  const [languageList, setLanguageList] = useState("");
-  const [selectedLanguage, setSelectedLanguage] = useState("");
+  const [languageList, setLanguageList] = useState([]);
+  const [selectedLanguage, setSelectedLanguage] = useState([]);
 
   const [spell, setSpell] = useState("");
   const [spells, setSpells] = useState("");
   const [spellOptions, setSpellOptions] = useState("");
-  const [spellList, setSpellList] = useState("");
-  const [selectedSpell, setSelectedSpell] = useState("");
+  const [spellList, setSpellList] = useState([]);
+  const [selectedSpell, setSelectedSpell] = useState([]);
 
   const [firstLvl, setFirstLvl] = useState("");
   const [firstLvls, setFirstLvls] = useState("");
   const [firstLvlOptions, setFirstLvlOptions] = useState("");
-  const [firstLvlList, setFirstLvlList] = useState("");
-  const [selectedFirstLvl, setSelectedFirstLvl] = useState("");
+  const [firstLvlList, setFirstLvlList] = useState([]);
+  const [selectedFirstLvl, setSelectedFirstLvl] = useState([]);
 
   const [secondLvl, setSecondLvl] = useState("");
   const [secondLvls, setSecondLvls] = useState("");
   const [secondLvlOptions, setSecondLvlOptions] = useState("");
-  const [secondLvlList, setSecondLvlList] = useState("");
-  const [selectedSecondLvl, setSelectedSecondLvl] = useState("");
+  const [secondLvlList, setSecondLvlList] = useState([]);
+  const [selectedSecondLvl, setSelectedSecondLvl] = useState([]);
 
   const [thirdLvl, setThirdLvl] = useState("");
   const [thirdLvls, setThirdLvls] = useState("");
   const [thirdLvlOptions, setThirdLvlOptions] = useState("");
-  const [thirdLvlList, setThirdLvlList] = useState("");
-  const [selectedThirdLvl, setSelectedThirdLvl] = useState("");
+  const [thirdLvlList, setThirdLvlList] = useState([]);
+  const [selectedThirdLvl, setSelectedThirdLvl] = useState([]);
 
   const [fourthLvl, setFourthLvl] = useState("");
   const [fourthLvls, setFourthLvls] = useState("");
   const [fourthLvlOptions, setFourthLvlOptions] = useState("");
-  const [fourthLvlList, setFourthLvlList] = useState("");
-  const [selectedFourthLvl, setSelectedFourthLvl] = useState("");
+  const [fourthLvlList, setFourthLvlList] = useState([]);
+  const [selectedFourthLvl, setSelectedFourthLvl] = useState([]);
 
   const [fifthLvl, setFifthLvl] = useState("");
   const [fifthLvls, setFifthLvls] = useState("");
   const [fifthLvlOptions, setFifthLvlOptions] = useState("");
-  const [fifthLvlList, setFifthLvlList] = useState("");
-  const [selectedFifthLvl, setSelectedFifthLvl] = useState("");
+  const [fifthLvlList, setFifthLvlList] = useState([]);
+  const [selectedFifthLvl, setSelectedFifthLvl] = useState([]);
 
   const [sixthLvl, setSixthLvl] = useState("");
   const [sixthLvls, setSixthLvls] = useState("");
   const [sixthLvlOptions, setSixthLvlOptions] = useState("");
-  const [sixthLvlList, setSixthLvlList] = useState("");
-  const [selectedSixthLvl, setSelectedSixthLvl] = useState("");
+  const [sixthLvlList, setSixthLvlList] = useState([]);
+  const [selectedSixthLvl, setSelectedSixthLvl] = useState([]);
 
   const [seventhLvl, setSeventhLvl] = useState("");
   const [seventhLvls, setSeventhLvls] = useState("");
   const [seventhLvlOptions, setSeventhLvlOptions] = useState("");
-  const [seventhLvlList, setSeventhLvlList] = useState("");
-  const [selectedSeventhLvl, setSelectedSeventhLvl] = useState("");
+  const [seventhLvlList, setSeventhLvlList] = useState([]);
+  const [selectedSeventhLvl, setSelectedSeventhLvl] = useState([]);
 
   const [eighthLvl, setEighthLvl] = useState("");
   const [eighthLvls, setEighthLvls] = useState("");
@@ -143,74 +143,74 @@ const ClassGen = () => {
   const [ninthLvl, setNinthLvl] = useState("");
   const [ninthLvls, setNinthLvls] = useState("");
   const [ninthLvlOptions, setNinthLvlOptions] = useState("");
-  const [ninthLvlList, setNinthLvlList] = useState("");
-  const [selectedNinthLvl, setSelectedNinthLvl] = useState("");
+  const [ninthLvlList, setNinthLvlList] = useState([]);
+  const [selectedNinthLvl, setSelectedNinthLvl] = useState([]);
 
   const [tenthLvl, setTenthLvl] = useState("");
   const [tenthLvls, setTenthLvls] = useState("");
   const [tenthLvlOptions, setTenthLvlOptions] = useState("");
-  const [tenthLvlList, setTenthLvlList] = useState("");
-  const [selectedTenthLvl, setSelectedTenthLvl] = useState("");
+  const [tenthLvlList, setTenthLvlList] = useState([]);
+  const [selectedTenthLvl, setSelectedTenthLvl] = useState([]);
 
   const [eleventhLvl, setEleventhLvl] = useState("");
   const [eleventhLvls, setEleventhLvls] = useState("");
   const [eleventhLvlOptions, setEleventhLvlOptions] = useState("");
-  const [eleventhLvlList, setEleventhLvlList] = useState("");
-  const [selectedEleventhLvl, setSelectedEleventhLvl] = useState("");
+  const [eleventhLvlList, setEleventhLvlList] = useState([]);
+  const [selectedEleventhLvl, setSelectedEleventhLvl] = useState([]);
 
   const [twelthLvl, setTwelthLvl] = useState("");
   const [twelthLvls, setTwelthLvls] = useState("");
   const [twelthLvlOptions, setTwelthLvlOptions] = useState("");
-  const [twelthLvlList, setTwelthLvlList] = useState("");
-  const [selectedTwelthLvl, setSelectedTwelthLvl] = useState("");
+  const [twelthLvlList, setTwelthLvlList] = useState([]);
+  const [selectedTwelthLvl, setSelectedTwelthLvl] = useState([]);
 
   const [thirteenthLvl, setThirteenthLvl] = useState("");
   const [thirteenthLvls, setThirteenthLvls] = useState("");
   const [thirteenthLvlOptions, setThirteenthLvlOptions] = useState("");
-  const [thirteenthLvlList, setThirteenthLvlList] = useState("");
-  const [selectedThirteenthLvl, setSelectedThirteenthLvl] = useState("");
+  const [thirteenthLvlList, setThirteenthLvlList] = useState([]);
+  const [selectedThirteenthLvl, setSelectedThirteenthLvl] = useState([]);
 
   const [fourteenthLvl, setFourteenthLvl] = useState("");
   const [fourteenthLvls, setFourteenthLvls] = useState("");
   const [fourteenthLvlOptions, setFourteenthLvlOptions] = useState("");
-  const [fourteenthLvlList, setFourteenthLvlList] = useState("");
-  const [selectedFourteenthLvl, setSelectedFourteenthLvl] = useState("");
+  const [fourteenthLvlList, setFourteenthLvlList] = useState([]);
+  const [selectedFourteenthLvl, setSelectedFourteenthLvl] = useState([]);
 
   const [fifteenthLvl, setFifteenthLvl] = useState("");
   const [fifteenthLvls, setFifteenthLvls] = useState("");
   const [fifteenthLvlOptions, setFifteenthLvlOptions] = useState("");
-  const [fifteenthLvlList, setFifteenthLvlList] = useState("");
-  const [selectedFifteenthLvl, setSelectedFifteenthLvl] = useState("");
+  const [fifteenthLvlList, setFifteenthLvlList] = useState([]);
+  const [selectedFifteenthLvl, setSelectedFifteenthLvl] = useState([]);
 
   const [sixteenthLvl, setSixteenthLvl] = useState("");
   const [sixteenthLvls, setSixteenthLvls] = useState("");
   const [sixteenthLvlOptions, setSixteenthLvlOptions] = useState("");
-  const [sixteenthLvlList, setSixteenthLvlList] = useState("");
-  const [selectedSixteenthLvl, setSelectedSixteenthLvl] = useState("");
+  const [sixteenthLvlList, setSixteenthLvlList] = useState([]);
+  const [selectedSixteenthLvl, setSelectedSixteenthLvl] = useState([]);
 
   const [seventeenthLvl, setSeventeenthLvl] = useState("");
   const [seventeenthLvls, setSeventeenthLvls] = useState("");
   const [seventeenthLvlOptions, setSeventeenthLvlOptions] = useState("");
-  const [seventeenthLvlList, setSeventeenthLvlList] = useState("");
-  const [selectedSeventeenthLvl, setSelectedSeventeenthLvl] = useState("");
+  const [seventeenthLvlList, setSeventeenthLvlList] = useState([]);
+  const [selectedSeventeenthLvl, setSelectedSeventeenthLvl] = useState([]);
 
   const [eighteenthLvl, setEighteenthLvl] = useState("");
   const [eighteenthLvls, setEighteenthLvls] = useState("");
   const [eighteenthLvlOptions, setEighteenthLvlOptions] = useState("");
-  const [eighteenthLvlList, setEighteenthLvlList] = useState("");
-  const [selectedEighteenthLvl, setSelectedEighteenthLvl] = useState("");
+  const [eighteenthLvlList, setEighteenthLvlList] = useState([]);
+  const [selectedEighteenthLvl, setSelectedEighteenthLvl] = useState([]);
 
   const [nineteenthLvl, setNineteenthLvl] = useState("");
   const [nineteenthLvls, setNineteenthLvls] = useState("");
   const [nineteenthLvlOptions, setNineteenthLvlOptions] = useState("");
-  const [nineteenthLvlList, setNineteenthLvlList] = useState("");
-  const [selectedNineteenthLvl, setSelectedNineteenthLvl] = useState("");
+  const [nineteenthLvlList, setNineteenthLvlList] = useState([]);
+  const [selectedNineteenthLvl, setSelectedNineteenthLvl] = useState([]);
 
   const [twentithLvl, setTwentithLvl] = useState("");
   const [twentithLvls, setTwentithLvls] = useState("");
   const [twentithLvlOptions, setTwentithLvlOptions] = useState("");
-  const [twentithLvlList, setTwentithLvlList] = useState("");
-  const [selectedTwentithLvl, setSelectedTwentithLvl] = useState("");
+  const [twentithLvlList, setTwentithLvlList] = useState([]);
+  const [selectedTwentithLvl, setSelectedTwentithLvl] = useState([]);
 
   //Show Options
   const showBasics = (e) => {
@@ -526,6 +526,19 @@ const ClassGen = () => {
                 list={skillProfList}
                 setList={setSkillProfList}
                 valueOptions={skillProfOptions}
+              />
+              <CustomDataTable
+                tableName={"itemsTypes"}
+                setSingular={setLanguage}
+                setPlural={setLanguages}
+                setOptions={setLanguageOptions}
+                h1Title={"Language Proficiencies"}
+                dialogHeader={"Language Proficiencies"}
+                selectedItem={selectedLanguage}
+                setSelectedItem={setSelectedLanguage}
+                list={languageList}
+                setList={setLanguageList}
+                valueOptions={languageOptions}
               />
             </div>
           </div>
@@ -893,11 +906,191 @@ const ClassGen = () => {
             </span>
           </h2>
           <h2>
-            Spell Options{" "}
+            Spell School{" "}
             <span className={style.minorText2}>
               <MultipleDisplay
-                selectedItem={selectedWeaponProf}
-                setList={setWeaponProfList}
+                selectedItem={selectedSpell}
+                setList={setSpellList}
+              />
+            </span>
+          </h2>
+          <h2>
+            1st Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedFirstLvl}
+                setList={setFirstLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            2nd Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedSecondLvl}
+                setList={setSecondLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            3rd Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedThirdLvl}
+                setList={setThirdLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            4th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedFourthLvl}
+                setList={setFourthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            5th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedFifthLvl}
+                setList={setFifthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            6th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedSixthLvl}
+                setList={setSixthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            7th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedSeventhLvl}
+                setList={setSeventhLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            8th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedEighthLvl}
+                setList={setEighthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            9th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedNinthLvl}
+                setList={setNinthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            10th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedTenthLvl}
+                setList={setTenthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            11th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedEleventhLvl}
+                setList={setEleventhLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            12th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedTwelthLvl}
+                setList={setTwelthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            13th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedThirteenthLvl}
+                setList={setThirteenthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            14th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedFourteenthLvl}
+                setList={setFourteenthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            15th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedFifteenthLvl}
+                setList={setFifteenthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            16th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedSixteenthLvl}
+                setList={setSixteenthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            17th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedSeventeenthLvl}
+                setList={setSeventeenthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            18th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedEighteenthLvl}
+                setList={setEighteenthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            19th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedNineteenthLvl}
+                setList={setNineteenthLvlList}
+              />
+            </span>
+          </h2>
+          <h2>
+            20th Level{" "}
+            <span className={style.minorText2}>
+              <MultipleDisplay
+                selectedItem={selectedTwentithLvl}
+                setList={setTwentithLvlList}
               />
             </span>
           </h2>
