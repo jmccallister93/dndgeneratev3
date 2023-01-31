0d69c8a39e1597a4a293eb0b029566efdace3009
jmccallister93 <jaredmccallister93@gmail.com>
2023-01-31 15:58:06 -0800

    Now passing through the whole object

--- src/notes/components/NoteTreeTable.js
+++ src/notes/components/NoteTreeTable.js
@@ -1,43 +1,11 @@
-import { Column } from "primereact/column";
-import { FilterMatchMode } from "primereact/api";
-import { DataTable } from "primereact/datatable";
-import { InputText } from "primereact/inputtext";
 import { useEffect, useState, useRef } from "react";
 import ns from "../../stylesheets/Note.module.scss";
-import { TreeTable } from "primereact/treetable";
 
 const NoteTreeTable = (props) => {
-  // function extractNames(node, names = []) {
-  //   console.log(node[0])
-  //   if (!node) {
-  //     console.log("no node");
-  //     return names;
-  //   }
-    
-  //   // Breaking right here
-  //   if (!node.data){
-  //     console.log("no data");
-  //     return names;
-  //   } 
-  //   //Never reaches here
-  //   console.log(node);
-  //   names.push(node.data.name);
-    
-  //   if (!node.children) {
-  //     console.log("no children");
-  //     return names;
-  //   }
-  //   for (const child of node.children) {
-  //     console.log(node);
-  //     extractNames(child, names);
-  //   }
-    
-  //   return names;
-  // }
   function extractNames(objectArray) {
     let names = [];
     for (let obj of objectArray) {
-      names.push(obj.data.name);
+      names.push({name: obj.data.name, key: obj.key});
       if (obj.children) {
         names = names.concat(extractNames(obj.children));
       }
@@ -45,17 +13,19 @@ const NoteTreeTable = (props) => {
     return names;
   }
   const names = extractNames(props.object);
+
+  const handleClick = (obj) => {
+    props.onSelectedItem(obj);
+  };
+
+
   return (
     <>
       <div className={ns.noteTreeCategory}>
-        {/* <div>{props.object[0].data.name}</div>
-        <div>{props.object[0].children[0].data.name}</div>
-        <div>{props.object[0].children[1].data.name}</div>
-        <div>{props.object[0].children[1].children[0].data.name}</div>
-        <div>{props.object[0].children[1].children[1].data.name}</div>
-        <div>{props.object[1].data.name}</div> */}
-        {names.map((name, index) => (
-          <div key={index}>{name}</div>
+        {names.map((obj, index) => (
+          <div 
+          key={index} 
+          onClick={() => handleClick(obj)}>{obj.name}</div>
         ))}
       </div>
     </>
@@ -63,3 +33,12 @@ const NoteTreeTable = (props) => {
 };
 
 export default NoteTreeTable;
+
+{
+  /* <div>{props.object[0].data.name}</div>
+        <div>{props.object[0].children[0].data.name}</div>
+        <div>{props.object[0].children[1].data.name}</div>
+        <div>{props.object[0].children[1].children[0].data.name}</div>
+        <div>{props.object[0].children[1].children[1].data.name}</div>
+        <div>{props.object[1].data.name}</div> */
+}
