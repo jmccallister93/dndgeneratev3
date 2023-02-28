5120088bb12e0fecc62b6b766d89f94729bed123
jmccallister93 <jaredmccallister93@gmail.com>
2023-02-28 11:10:15 -0800

    Cannot get the updates to fire

--- src/notes/components/Note.js
+++ src/notes/components/Note.js
@@ -8,36 +8,39 @@ function Note(props) {
 
   useEffect(() => {
     if (props.selectedNode && props.selectedNode.notes) {
-      setNotes(props.selectedNode.notes);
-      setNotes("")
+      setAllNotes(props.selectedNode.notes.split("\n"));
     } else {
-      setNotes("");
+      setAllNotes([]);
     }
+    console.log("props")
   }, [props.selectedNode]);
+
+  useEffect(() => {
+    console.log("fired")
+  }, [props.updateNote])
 
   const handleNotesChange = (e) => {
     setNotes(e.target.value);
   };
 
- useEffect(() => {
-      console.log()
- }, [notes])
-
   const handleAddNotes = () => {
-    const newNotesArray = notes.split("\n").filter((note) => note.trim() !== "");
+    const newNotesArray = notes
+      .split("\n")
+      .filter((note) => note.trim() !== "");
     setAllNotes([...allNotes, ...newNotesArray]);
     setSelectedNoteIndex(allNotes.length);
     props.updateNote([...allNotes, ...newNotesArray].join("\n"));
     setNotes("");
+    setSelectedNoteIndex(-1);
   };
 
-  useEffect(() => {
-    if (props.selectedNode?.notes) {
-      setAllNotes(props.selectedNode.notes.split("\n"));
-    } else {
-      setAllNotes([]);
+  const handleEnterDown = (event) => {
+    if (event.key === "Enter") {
+      setSelectedNoteIndex(-1);
+      setAllNotes(allNotes.map((note, index) => index === selectedNoteIndex ? event.target.value : note));
+      props.updateNote(allNotes.join("\n"));
     }
-  }, [props.selectedNode]);
+  };
 
   return (
     <>
@@ -65,30 +68,32 @@ function Note(props) {
             {props.selectedNode && Object.keys(props.selectedNode).length ? (
               <ul>
                 {allNotes.map((note, index) => (
-                  // <li key={index}>- {note}</li>
                   <li key={index}>
-                  {selectedNoteIndex === index ? (
-                    <input
-                      type="text"
-                      value={note}
-                      onChange={(e) => {
-                        const newNotes = [...allNotes];
-                        newNotes[index] = e.target.value;
-                        setAllNotes(newNotes);
-                      }}
-                      onBlur={() => {
-                        setSelectedNoteIndex(-1)
-                        props.updateNote(allNotes.join("\n"));
-                      }}
-                      autoFocus
-                    />
-                  ) : (
-                    <>
-                      <span>- {note}</span>
-                      <button onClick={() => setSelectedNoteIndex(index)}>Edit</button>
-                    </>
-                  )}
-                </li>
+                    {selectedNoteIndex === index ? (
+                      <input
+                        type="text"
+                        value={note}
+                        onChange={(e) => {
+                          const newNotes = [...allNotes];
+                          newNotes[index] = e.target.value;
+                          setAllNotes(newNotes);
+                        }}
+                        onBlur={() => {
+                          setSelectedNoteIndex(-1);
+                          props.updateNote(allNotes.join("\n"));
+                        }}
+                        onKeyDown={handleEnterDown}
+                        autoFocus
+                      />
+                    ) : (
+                      <>
+                        <span>- {note}</span>
+                        <button onClick={() => setSelectedNoteIndex(index)}>
+                          Edit
+                        </button>
+                      </>
+                    )}
+                  </li>
                 ))}
               </ul>
             ) : null}
