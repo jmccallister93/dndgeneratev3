62d03dc2a0ed1f397651aaffca454fff946b1fe4
jmccallister93 <jaredmccallister93@gmail.com>
2023-03-24 14:45:33 -0700

    Data persisting but not live rendering

--- src/notes/components/NodeList.js
+++ src/notes/components/NodeList.js
@@ -3,26 +3,11 @@ import ns from "../../stylesheets/Note.module.scss";
 import style from "../../stylesheets/PageStyle.module.scss";
 
 const NodeList = (props) => {
-  const [links, setLinks] = useState("");
   const [allLinks, setAllLinks] = useState([]);
-  const [linkedNode, setLinkedNode] = useState("");
-
-  //   const handleLinkNode = async (nodeName) => {
-  //     try {
-  //       setLinks([props.selectedNode.links]);
-  //       setAllLinks([...allLinks, nodeName]);
-  //       const newLinksArray = allLinks.join(", ");
-  //       const updatedNode = { ...props.selectedNode, links: newLinksArray };
-  //       await props.updateSelectedNode(updatedNode);
-  //       props.setPropertyValue(nodeName);
-  //     } catch (error) {
-  //       console.error("Error updating note:" + error);
-  //     }
-  //   };
 
   useEffect(() => {
     setAllLinks([props.selectedNode.links]);
-  }, [props.selectedNode.links])
+  }, [props.selectedNode.links]);
 
   const handleLinkNode = (nodeName) => {
     setAllLinks([...allLinks, nodeName]);
@@ -31,8 +16,6 @@ const NodeList = (props) => {
     props.setPropertyValue(newLinksArray);
     console.log(newLinksArray);
   };
-
-;
 
   return (
     <div className={ns.nodeListWrapper}>
@@ -51,25 +34,49 @@ const NodeList = (props) => {
       <div className={ns.nodeList}>
         <h2>NPCs:</h2>
         {props.npc.map((node) => (
-          <div key={node.id}>{node.name}</div>
+          <div
+            className={ns.linkItem}
+            key={node.uuid}
+            onClick={() => handleLinkNode(node.name)}
+          >
+            {node.name}
+          </div>
         ))}
       </div>
       <div className={ns.nodeList}>
         <h2>Organizations:</h2>
         {props.organization.map((node) => (
-          <div key={node.id}>{node.name}</div>
+          <div
+            className={ns.linkItem}
+            key={node.uuid}
+            onClick={() => handleLinkNode(node.name)}
+          >
+            {node.name}
+          </div>
         ))}
       </div>
       <div className={ns.nodeList}>
         <h2>Quests:</h2>
         {props.quest.map((node) => (
-          <div key={node.id}>{node.name}</div>
+          <div
+            className={ns.linkItem}
+            key={node.uuid}
+            onClick={() => handleLinkNode(node.name)}
+          >
+            {node.name}
+          </div>
         ))}
       </div>
       <div className={ns.nodeList}>
         <h2>Items:</h2>
         {props.item.map((node) => (
-          <div key={node.id}>{node.name}</div>
+          <div
+            className={ns.linkItem}
+            key={node.uuid}
+            onClick={() => handleLinkNode(node.name)}
+          >
+            {node.name}
+          </div>
         ))}
       </div>
     </div>
